c99bbc344614872e7994d16a9cfe8792684603a3
watcharakornton <watcharakorn.jan@gmail.com>
2020-09-17 20:37:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        input: "",
+        output: ""
+    });
+});
+
+
+app.post('/go', (req, res) => {
+    let input = req.body.input;
+    let type = typeof input;   
+    let output = input.replace(/[a-z]/g, "");
+    console.log(`input: ${input} ,type input is ${type}`);
+    console.log(`output: ${output}`)
+    res.render('index.ejs', {
+        output: output,
+        input: input
+    })
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
